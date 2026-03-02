97da8149d571551dba28dab208fa257cc0f387e4
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-02 23:37:34 +0200

    refactor(customer): Update routing global loader to use the global shared spinner component

--- src/app/components/routing-global-loader/routing-global-loader.ts
+++ src/app/components/routing-global-loader/routing-global-loader.ts
@@ -1,35 +1,27 @@
 import { Component } from '@angular/core';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { RouterLoadingService } from '../../core/services/routing-loading.service';
-import { ProgressBar } from "primeng/progressbar";
+import { ProgressBar } from 'primeng/progressbar';
+import { Spinner } from '../../shared/components/spinner/spinner';
 
 @Component({
   selector: 'app-routing-global-loader',
-  imports: [ProgressSpinnerModule, ProgressBar],
+  imports: [ProgressSpinnerModule, ProgressBar, Spinner],
   templateUrl: './routing-global-loader.html',
   styleUrl: './routing-global-loader.scss',
 })
-export class RoutingGlobalLoader  {
+export class RoutingGlobalLoader {
+  constructor(public routerLoadingService: RouterLoadingService) {}
 
-
-  constructor(public routerLoadingService : RouterLoadingService){
-    
-  }
-
-  getCurrentProgress(){
-
-    if(this.routerLoadingService.phase() === 'guards'){
+  getCurrentProgress() {
+    if (this.routerLoadingService.phase() === 'guards') {
       return 30;
-    }else if(this.routerLoadingService.phase() === 'resolving'){
+    } else if (this.routerLoadingService.phase() === 'resolving') {
       return 60;
-    }
-    else if(this.routerLoadingService.phase() === 'activating'){
+    } else if (this.routerLoadingService.phase() === 'activating') {
       return 90;
-    }else{
+    } else {
       return 100;
     }
-
-   
-   
   }
 }
