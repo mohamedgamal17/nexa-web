40f01f036bd904dcbb7d361bcd8afc94319bd0eb
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-02 06:53:42 +0200

    feat(customer): Add base public layout

---
+++ src/app/layouts/public-layout/public-layout.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [],
+  templateUrl: './public-layout.html',
+})
+export class PublicLayout {}
