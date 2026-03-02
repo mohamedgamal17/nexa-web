a90638ede5ce37c4b40f4b14c01a3b485c1e9aa2
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-02 23:36:46 +0200

    feat(customer): Add global shared spinner component

---
+++ src/app/shared/components/spinner/spinner.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
+@Component({
+  selector: 'app-spinner',
+  imports: [ProgressSpinnerModule],
+  templateUrl: './spinner.html',
+  styleUrl: './spinner.scss',
+})
+export class Spinner {
+  text = input<string>();
+}
