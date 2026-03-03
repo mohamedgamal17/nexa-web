a95c714d6ab0bf6cc400c015ab7e6c13b2f0a4c2
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-03 06:48:18 +0200

    feat(customer): Add api interaction to profile-contact component

--- src/app/features/customers/components/profile-contact/profile-contact.ts
+++ src/app/features/customers/components/profile-contact/profile-contact.ts
@@ -1,28 +1,87 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, effect, inject, input, OnInit, output, signal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { Button } from 'primeng/button';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { featherMail } from '@ng-icons/feather-icons';
 import { heroPhone } from '@ng-icons/heroicons/outline';
 import { InputTextModule } from 'primeng/inputtext';
+import { parsePhoneNumber } from '../../../../shared/utils/phone-number.utils';
+import { PhoneNumberValidators } from '../../../../shared/components/phone-input/validators/phone-input.validator';
+import { FormError } from '../../../../shared/components/form-error/form-error';
+import { InputError } from '../../../../shared/components/input-error/Input-error';
+import { TranslateModule } from '@ngx-translate/core';
+import { PhoneInput } from '../../../../shared/components/phone-input/phone-input';
+import { PhoneValueModel } from '../../../../shared/components/phone-input/models/phone-value-model.interface';
 
 @Component({
   selector: 'app-profile-contact',
-  imports: [CardModule, FormsModule, Button, NgIcon, InputTextModule],
+  imports: [CardModule, FormsModule, Button, NgIcon, InputTextModule, ReactiveFormsModule, InputError, TranslateModule, PhoneInput],
   templateUrl: './profile-contact.html',
   styleUrl: './profile-contact.scss',
   viewProviders: [provideIcons({ featherMail, heroPhone })],
 })
-export class ProfileContact {
-  dummy = signal({
-    email: 'john.doe@example.com',
-    phone: '+1 555 123 4567',
-  });
+export class ProfileContact implements OnInit {
+  contact = input<{ email: string; phoneNumber: string }>();
+  emailSubmiting = input(false);
+  phoneSubmiting = input(false);
+  fb = inject(FormBuilder);
+
+  emailForm: FormGroup;
+  phoneForm: FormGroup;
 
   editEmail = signal(false);
-
   editPhone = signal(false);
+
+  emailSubmited = output<{ email: string }>();
+  phoneSubmited = output<{ phoneNumber: PhoneValueModel }>();
+
+  constructor() {}
+  ngOnInit(): void {
+    this.emailForm = this.fb.group({
+      email: ['', [Validators.required, Validators.maxLength(256), Validators.email]],
+    });
+
+    this.phoneForm = this.fb.group({
+      phoneNumber: [, [Validators.required, PhoneNumberValidators.phoneRequired(), PhoneNumberValidators.phoneMinLength(6), PhoneNumberValidators.phoneMaxLength(20), PhoneNumberValidators.phoneValid()]],
+    });
+    var value = this.contact();
+
+    if (!value) {
+      return;
+    }
+
+    if (value.email) {
+      this.emailForm.patchValue({ email: value.email });
+    }
+
+    if (value.phoneNumber) {
+      const model = parsePhoneNumber(value.phoneNumber);
+      this.phoneForm.patchValue({ phoneNumber: model });
+    }
+  }
+
+  handleEmailSumbit($event: SubmitEvent) {
+    $event.preventDefault();
+    if (this.emailForm.valid) {
+      if (this.emailForm.value.email == this.contact()?.email) {
+        this.toggleEditEmail();
+      } else {
+        this.emailSubmited.emit(this.emailForm.value);
+      }
+    }
+  }
+
+  handlePhoneSubmit($event: SubmitEvent) {
+    $event.preventDefault();
+    if (this.phoneForm.valid) {
+      if (this.phoneForm.value.phoneNumber?.international === this.contact()?.phoneNumber) {
+        this.toggleEditPhone();
+      } else {
+        this.phoneSubmited.emit(this.phoneForm.value);
+      }
+    }
+  }
 
   toggleEditEmail() {
     this.editEmail.set(!this.editEmail());
--- src/app/features/customers/pages/profile-page/profile-page.ts
+++ src/app/features/customers/pages/profile-page/profile-page.ts
@@ -12,24 +12,33 @@ import { Customer } from '../../interfaces/customer.interface';
 import { Spinner } from '../../../../shared/components/spinner/spinner';
 import { DataLoaderError } from '../../../../shared/components/data-loader-error/data-loader-error';
 import { customerActions } from '../../state/customer.actions';
+import { PhoneValueModel } from '../../../../shared/components/phone-input/models/phone-value-model.interface';
+import { CustomerService } from '../../services/customer.service';
+import { MessageService } from 'primeng/api';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [ProfileHero, ProfileContact, ProfileInfo, ProfileAddress, ProfileDocument, Spinner, DataLoaderError],
+  imports: [ProfileHero, ProfileContact, ProfileInfo, ProfileAddress, ProfileDocument, Spinner, DataLoaderError, ToastModule],
+  providers: [MessageService],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.scss',
 })
 export class ProfilePage implements OnInit {
   private store: Store = inject(Store);
-
+  private customerService = inject(CustomerService);
+  private messageService = inject(MessageService);
+  private translateSevice = inject(TranslateService);
   customer = signal<Customer | null>(null);
   isLoading = signal(false);
   apiError = signal<ErrorModel | null>(null);
   hasApiError = computed(() => this.apiError() != null);
 
-  constructor() {
-    effect(() => console.log(this.apiError()));
-  }
+  isSubmitingPhone = signal(false);
+  isSubmitingEmail = signal(false);
+
+  constructor() {}
   ngOnInit(): void {
     this.loadCustomer();
   }
@@ -39,6 +48,59 @@ export class ProfilePage implements OnInit {
     this.store.select(selectIsCustomerLoading).subscribe((value) => this.isLoading.set(value));
     this.store.select(selectCustomerError).subscribe((value) => this.apiError.set(value));
     this.store.select(selectCustomer).subscribe((value) => this.customer.set(value));
+  }
+
+  handleEmailSubmit($event: { email: string }) {
+    this.isSubmitingEmail.set(true);
+    this.customerService.updateEmail({ emailAddress: $event.email }).subscribe({
+      next: (value) => {
+        this.store.dispatch(customerActions.updateCustomer({ customer: value }));
+        this.messageService.add({
+          summary: this.translateSevice.instant('toast.emailSaved.summary'),
+          detail: this.translateSevice.instant('toast.emailSaved.message'),
+          severity: 'success',
+        });
+        this.isSubmitingEmail.set(false);
+      },
+      error: (error: ErrorModel) => {
+        this.messageService.add({
+          summary: error.title,
+          detail: error.message,
+          severity: 'error',
+        });
+        this.isSubmitingEmail.set(false);
+      },
+    });
+  }
+
+  handlePhoneSubmit($event: { phoneNumber: PhoneValueModel }) {
+    this.isSubmitingPhone.set(true);
+    this.customerService.updatePhone({ phoneNumber: $event.phoneNumber.international }).subscribe({
+      next: (value) => {
+        this.store.dispatch(customerActions.updateCustomer({ customer: value }));
+        this.messageService.add({
+          summary: this.translateSevice.instant('toast.phoneSaved.summary'),
+          detail: this.translateSevice.instant('toast.phoneSaved.message'),
+          severity: 'success',
+        });
+        this.isSubmitingPhone.set(false);
+      },
+      error: (error: ErrorModel) => {
+        this.messageService.add({
+          summary: error.title,
+          detail: error.message,
+          severity: 'error',
+        });
+        this.isSubmitingPhone.set(false);
+      },
+    });
+  }
+
+  extractContact() {
+    return {
+      email: this.customer()?.emailAddress ?? '',
+      phoneNumber: this.customer()?.phoneNumber ?? '',
+    };
   }
 
   reloadPage() {
