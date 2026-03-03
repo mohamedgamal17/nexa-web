d8cd6d5d1b44f79c16a09eaf3e05ac5d9590277f
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-03 06:47:11 +0200

    feat(customer): Add implementation to update email and phone at the api

--- src/app/features/customers/services/customer.service.ts
+++ src/app/features/customers/services/customer.service.ts
@@ -1,27 +1,29 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Customer } from "../interfaces/customer.interface";
-import { environemnt } from "../../../../environments/environemnt";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Customer } from '../interfaces/customer.interface';
+import { environemnt } from '../../../../environments/environemnt';
 
 @Injectable({
-  providedIn : "root"
+  providedIn: 'root',
 })
-export class CustomerService{
+export class CustomerService {
+  private readonly apiUrl = environemnt.apiUrl + '/user/customer';
 
-  private readonly apiUrl = environemnt.apiUrl +  "/user/customer"
+  constructor(private readonly httpClient: HttpClient) {}
 
-  constructor(private readonly httpClient : HttpClient){
-
+  getCustomer() {
+    return this.httpClient.get<Customer>(this.apiUrl);
   }
 
-  getCustomer(){
-    return this.httpClient.get<Customer>(this.apiUrl)
+  updateDocument(req: { kycDocumentId?: string; issuingCountry?: string; type?: DocumentType }) {
+    return this.httpClient.post<Customer>(this.apiUrl + '/document', req);
   }
 
-
-  updateDocument(req : {kycDocumentId? : string , issuingCountry? : string, type? : DocumentType}){
-    return this.httpClient.post<Customer>(this.apiUrl + "/document", req)
+  updateEmail(req: { emailAddress: string }) {
+    return this.httpClient.post<Customer>(this.apiUrl + '/email', req);
   }
 
-
+  updatePhone(req: { phoneNumber: string }) {
+    return this.httpClient.post<Customer>(this.apiUrl + '/phone', req);
+  }
 }
