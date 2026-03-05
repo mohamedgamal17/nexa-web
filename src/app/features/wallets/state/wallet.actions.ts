b9a2b37d8ea50e7f661496c968b5dbe2089e61d3
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-05 19:53:17 +0200

    refactor(wallets): Update wallet action param type

--- src/app/features/wallets/state/wallet.actions.ts
+++ src/app/features/wallets/state/wallet.actions.ts
@@ -2,6 +2,7 @@ import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { Wallet } from '../interfaces/wallet.interface';
 import { PagingRequest } from '../../../core/models/paging-request.interface';
 import { PagingInfoResponse } from '../../../core/models/paging-response.interface';
+import { ErrorModel } from '../../../core/models/error-model.interface';
 
 export const walletActions = createActionGroup({
   source: '[Wallet]',
@@ -9,6 +10,6 @@ export const walletActions = createActionGroup({
     'set Active Wallet': props<{ wallet: Wallet }>(),
     'Load Wallets': props<{ paging: PagingRequest }>(),
     'Load Wallets Success': props<{ wallets: Wallet[]; paging: PagingInfoResponse }>(),
-    'Load Wallets Failure': props<{ error: string }>(),
+    'Load Wallets Failure': props<{ error: ErrorModel }>(),
   },
 });
