c93ee303910cae7995033fc88341ffa6c30fcc86
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-07 19:26:23 +0200

    feat(wallet): Add deposit and withdraw buttons with modal functionality

--- src/app/features/wallets/components/wallet-card/wallet-card.ts
+++ src/app/features/wallets/components/wallet-card/wallet-card.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, signal } from '@angular/core';
+import { Component, input, model, signal } from '@angular/core';
 import { Card } from 'primeng/card';
 import { Button } from 'primeng/button';
-import { heroArrowDownLeft, heroArrowUpRight } from '@ng-icons/heroicons/outline';
+import { heroArrowDownLeft, heroArrowUpRight, heroMinus, heroPlus } from '@ng-icons/heroicons/outline';
 import { LUCIDE_ICONS, LucideAngularModule, LucideIconProvider, Plus, Wallet as WalletIcon } from 'lucide-angular';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { SkeletonModule } from 'primeng/skeleton';
@@ -13,10 +13,24 @@ import { Wallet } from '../../interfaces/wallet.interface';
   imports: [Card, CommonModule, LucideAngularModule, NgIcon, SkeletonModule],
   templateUrl: './wallet-card.html',
   styleUrl: './wallet-card.scss',
-  viewProviders: [provideIcons({ heroArrowUpRight, heroArrowDownLeft })],
+  viewProviders: [provideIcons({ heroArrowUpRight, heroArrowDownLeft, heroPlus, heroMinus })],
 })
 export class WalletCard {
   loading = input<boolean>(true);
   wallet = input<Wallet>();
   WalletIcon = WalletIcon;
+
+  transferModelView = model(false);
+  transferBankView = model(false);
+  transferBankType = model<'withdraw' | 'deposit'>('withdraw');
+
+  showDepositModal() {
+    this.transferBankView.set(true);
+    this.transferBankType.set('deposit');
+  }
+
+  showWithdrawModal() {
+    this.transferBankView.set(true);
+    this.transferBankType.set('withdraw');
+  }
 }
