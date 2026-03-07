588db560b7db7dd2397858bdf69220003c11cc6c
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-07 19:27:39 +0200

    refactor(transfer): Standardize formatting and import order in transfer interface

--- src/app/features/transfers/interfaces/transfer.interface.ts
+++ src/app/features/transfers/interfaces/transfer.interface.ts
@@ -1,22 +1,22 @@
-import { Entity } from "../../../core/interfaces/entity.interface"
-import { BankTransfer } from "../../banks/enums/bank-transfer.enum"
-import { Wallet } from "../../wallets/interfaces/wallet.interface"
-import { TransferDirection } from "../enums/transfer-direction.enum"
-import { TransferStatus } from "../enums/transfer-status.enum"
-import { TransferType } from "../enums/transfer-type.enum"
+import { Entity } from '../../../core/models/entity.interface';
+import { Wallet } from '../../wallets/interfaces/wallet.interface';
+import { TransferDirection } from '../enums/transfer-direction.enum';
+import { TransferStatus } from '../enums/transfer-status.enum';
+import { TransferType } from '../enums/transfer-type.enum';
+import { BankTransfer } from './bank-transfer-request.interface';
 
-export interface Transfer extends Entity{
-  userId: string
-  walletId: string
-  wallet: Wallet
-  number: string
-  amount: number
-  reciverId? : string
-  reciver?: Wallet
-  fundingResourceId? : string
-  direction : TransferDirection
-  bankTransferType? : BankTransfer
-  status: TransferStatus
-  completedAt: Date
-  type: TransferType
+export interface Transfer extends Entity {
+  userId: string;
+  walletId: string;
+  wallet: Wallet;
+  number: string;
+  amount: number;
+  reciverId?: string;
+  reciver?: Wallet;
+  fundingResourceId?: string;
+  direction: TransferDirection;
+  bankTransferType?: BankTransfer;
+  status: TransferStatus;
+  completedAt: Date;
+  type: TransferType;
 }
