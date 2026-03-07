6e61f71836aa9560518eeb6e8ae2c083a847f2a9
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-08 01:43:40 +0200

    feat(bank): Add BankService for managing bank data retrieval

--- src/app/features/banks/services/bank.service.ts
+++ src/app/features/banks/services/bank.service.ts
@@ -1,11 +1,11 @@
 import { Observable } from 'rxjs';
-import { PagingRequest } from '../../../../core/models/paging-request.interface';
-import { PagingResponse } from '../../../../core/models/paging-response.interface';
+import { PagingRequest } from '../../../core/models/paging-request.interface';
+import { PagingResponse } from '../../../core/models/paging-response.interface';
 import { HttpClient } from '@angular/common/http';
-import { environemnt } from '../../../../../environments/environemnt';
+import { environemnt } from '../../../../environments/environemnt';
 import { Injectable } from '@angular/core';
-import { Bank } from '../bank.interface';
-import { mapObjectToHttpParam } from '../../../../core/mappers/http.mapper';
+import { Bank } from '../interfaces/bank.interface';
+import { mapObjectToHttpParam } from '../../../core/mappers/http.mapper';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +16,9 @@ export class BankService {
   constructor(private httpClient: HttpClient) {}
 
   getAllBanks(req: PagingRequest): Observable<PagingResponse<Bank>> {
-    return this.httpClient.get<PagingResponse<Bank>>(this.apiUrl, { params: mapObjectToHttpParam(req) });
+    return this.httpClient.get<PagingResponse<Bank>>(this.apiUrl, {
+      params: mapObjectToHttpParam(req),
+    });
   }
 
   getBankById(id: string): Observable<Bank> {
