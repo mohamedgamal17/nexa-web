c947e89cb50ef756238c73856868234a71aa6b35
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-08 07:16:19 +0200

    refactor(wallet): Add walletCardReducer to store configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
@@ -8,24 +11,37 @@ import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
 import { environemnt } from '../environments/environemnt';
 import { provideOnboardingFeature } from './features/onboarding/onboarding.feature';
 import { provideTranslateService } from '@ngx-translate/core';
-import { provideTranslateHttpLoader, TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  provideTranslateHttpLoader,
+  TranslateHttpLoader,
+} from '@ngx-translate/http-loader';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { errorHandlerInterceptorFn } from './core/interceptors/error-handler.interceptor';
 import { provideCustomers as provideCustomersFeature } from './features/customers/customers.feature';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import { CUSTOMER_KEY_FEAUTRE, customerReducer } from './features/customers/state/customer.reducer';
+import {
+  CUSTOMER_KEY_FEAUTRE,
+  customerReducer,
+} from './features/customers/state/customer.reducer';
 import { CustomerEffect } from './features/customers/state/customer.effect';
 import { provideLanding } from './features/landing/landing.feature';
-import { WALLET_FEATURE_KEY, walletReducer } from './features/wallets/state/wallet.reducer';
+import {
+  WALLET_CARD_FEATURE_KEY,
+  WALLET_FEATURE_KEY,
+  walletCardReducer,
+  walletReducer,
+} from './features/wallets/state/wallet.reducer';
 import { WalletEffects } from './features/wallets/state/wallet.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorHandlerInterceptorFn, authHttpInterceptorFn])),
+    provideHttpClient(
+      withInterceptors([errorHandlerInterceptorFn, authHttpInterceptorFn]),
+    ),
     provideAuth0({
       domain: environemnt.auth0.domain,
       clientId: environemnt.auth0.clientId,
@@ -67,6 +83,7 @@ export const appConfig: ApplicationConfig = {
     provideStore({
       [CUSTOMER_KEY_FEAUTRE]: customerReducer,
       [WALLET_FEATURE_KEY]: walletReducer,
+      [WALLET_CARD_FEATURE_KEY]: walletCardReducer,
     }),
     provideEffects([CustomerEffect, WalletEffects]),
   ],
