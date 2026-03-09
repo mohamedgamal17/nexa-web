e8657bb50801100c27d5ab20439ff16fd427d3ce
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-09 05:01:37 +0200

    fix(wallet): Fix error type extracting in wallet ngrx state

--- src/app/features/wallets/state/wallet.effects.ts
+++ src/app/features/wallets/state/wallet.effects.ts
@@ -1,5 +1,10 @@
 import { inject, Injectable } from '@angular/core';
-import { Actions, createEffect, ofType, ROOT_EFFECTS_INIT } from '@ngrx/effects';
+import {
+  Actions,
+  createEffect,
+  ofType,
+  ROOT_EFFECTS_INIT,
+} from '@ngrx/effects';
 import { walletActions } from './wallet.actions';
 import { catchError, map, mergeMap, of, tap } from 'rxjs';
 import { UserWalletService } from '../services/user-wallet-service.service';
@@ -15,15 +20,22 @@ export class WalletEffects {
     return this.actions.pipe(
       ofType(walletActions.loadWallets),
       mergeMap(({ paging }) =>
-        this.userWalletService.getAllWallets({ skip: paging?.skip ?? 0, length: paging?.length ?? 10 }).pipe(
-          map((resp) =>
-            walletActions.loadWalletsSuccess({
-              wallets: resp.data,
-              paging: resp.info,
-            }),
+        this.userWalletService
+          .getAllWallets({
+            skip: paging?.skip ?? 0,
+            length: paging?.length ?? 10,
+          })
+          .pipe(
+            map(resp =>
+              walletActions.loadWalletsSuccess({
+                wallets: resp.data,
+                paging: resp.info,
+              }),
+            ),
+            catchError(err =>
+              of(walletActions.loadWalletsFailure({ error: err })),
+            ),
           ),
-          catchError((err) => of(walletActions.loadWalletsFailure({ error: err.error }))),
-        ),
       ),
     );
   });
