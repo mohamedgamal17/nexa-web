06f9e005ec461c3e6d9df6f83fa03cad0039736b
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-26 21:53:51 +0200

    test(profile): Add profile component unit test

--- src/app/features/customers/components/profile-address/profile-address.spec.ts
+++ src/app/features/customers/components/profile-address/profile-address.spec.ts
@@ -1,22 +1,94 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileAddress } from './profile-address';
+import { Address } from '../../interfaces/address.interface';
+import { COUNTRIES } from '../../../../core/constants/countries.data';
+import { getStatesByCountry } from '../../../../core/constants/states.data';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('ProfileAddress', () => {
   let component: ProfileAddress;
   let fixture: ComponentFixture<ProfileAddress>;
 
+  const inputAddress: Address = {
+    country: 'US',
+    state: 'CA',
+    city: 'Los Angeles',
+    streetLine: '123 Sunset Blvd',
+    postalCode: '90001',
+    zipCode: '90001',
+  };
+
+  const formAddress: Address = {
+    country: 'US',
+    state: 'NY',
+    city: 'New York',
+    streetLine: '456 Madison Ave',
+    postalCode: '10001',
+    zipCode: '10001',
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProfileAddress],
+      imports: [ProfileAddress, TranslateModule.forRoot({})],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileAddress);
     component = fixture.componentInstance;
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should toggle edit address when toggled edit address is called', () => {
+    const currentEditAddress = component.editAddress();
+
+    component.toggleEditAddress();
+
+    expect(component.editAddress()).toBe(!currentEditAddress);
+  });
+
+  it('should emit submited event when handle submit is called', () => {
+    const spyOnSubmited = vi.spyOn(component.submited, 'emit');
+
+    component.addressForm.patchValue({
+      ...formAddress,
+    });
+
+    console.log(component.addressForm.value)
+
+    component.handleSubmit(new Event('submit') as any);
+
+    expect(spyOnSubmited).toHaveBeenCalledOnce();
+  });
+
+  it('should set selected country value when handle country change is called', () => {
+    const selectedCountry = COUNTRIES[0];
+
+    component.handleCountryChange(selectedCountry.code);
+
+    expect(component.currentSelectedCountry).toBeTruthy();
+    expect(component.currentSelectedCountry()).toBe(selectedCountry.code);
+  });
+
+  it('should set current country filter state according to current selected coutnry', () => {
+    const selectedCountry = COUNTRIES[0];
+    const expectedStateLength = getStatesByCountry(selectedCountry.code)
+    component.handleCountryChange(selectedCountry.code);
+
+    expect(component.states()).toBe(expectedStateLength)
+  });
+
+  it('should not emit the submit event if the form address same as input address', ()=>{
+
+    const spyonSubmited = vi.spyOn(component.submited, 'emit')
+
+    fixture.componentRef.setInput("address", inputAddress)
+
+    component.handleSubmit(new Event('submit') as any)
+
+    expect(spyonSubmited).not.toHaveBeenCalled()
+  })
 });
--- src/app/features/customers/components/profile-info/profile-info.spec.ts
+++ src/app/features/customers/components/profile-info/profile-info.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileInfo } from './profile-info';
+import { TranslateModule } from '@ngx-translate/core';
+import { Gender } from '../../enums/gender.enum';
 
 describe('ProfileInfo', () => {
   let component: ProfileInfo;
   let fixture: ComponentFixture<ProfileInfo>;
 
+  const inputInfo = {
+    firstName : "simple",
+    lastName : "test",
+    gender : Gender.Female,
+    birthDate : "1/10/1998"
+  }
+  const formInfo = {
+    firstName : "jhon",
+    lastName : "doe",
+    gender : Gender.Male,
+    birthDate : "1/10/2001"
+  }
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProfileInfo],
+      imports: [ProfileInfo, TranslateModule.forRoot({})],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileInfo);
@@ -19,4 +33,41 @@ describe('ProfileInfo', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit info submited when handle submit is called', ()=>{
+
+    const spyOnSubmited = vi.spyOn(component.submited, 'emit')
+
+    component.infoForm.patchValue({
+      ...formInfo
+    })
+
+    component.handleSubmit(new Event('submit') as any)
+
+    expect(spyOnSubmited).toHaveBeenCalledOnce()
+  })
+
+  it('should toggle edit profile value when toogle edit from is called',()=>{
+    const currentValue = component.editProfile()
+
+    component.toggleEditProfile()
+
+    expect(component.editProfile()).toBe(!currentValue)
+  })
+
+
+    it('should not emit info submited when form info is same as input info', ()=>{
+
+    const spyOnSubmited = vi.spyOn(component.submited, 'emit')
+
+    fixture.componentRef.setInput('info' , formInfo)
+
+    component.infoForm.patchValue({
+      ...formInfo
+    })
+
+    component.handleSubmit(new Event('submit') as any)
+
+    expect(spyOnSubmited).not.toHaveBeenCalledOnce()
+  })
 });
