706eede91190774248db7a7a0de6955d91295396
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-26 21:53:20 +0200

    fix(profile): Fix profile address component submiting optimization

--- src/app/features/customers/components/profile-address/profile-address.ts
+++ src/app/features/customers/components/profile-address/profile-address.ts
@@ -1,4 +1,12 @@
-import { Component, computed, inject, input, OnInit, output, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  OnInit,
+  output,
+  signal,
+} from '@angular/core';
 import { COUNTRIES } from '../../../../core/constants/countries.data';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
@@ -6,7 +14,13 @@ import { SelectModule } from 'primeng/select';
 import { InputTextModule } from 'primeng/inputtext';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { featherMapPin } from '@ng-icons/feather-icons';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { getStatesByCountry } from '../../../../core/constants/states.data';
 import { Address } from '../../interfaces/address.interface';
@@ -16,7 +30,18 @@ import { InputError } from '../../../../shared/components/input-error/Input-erro
 
 @Component({
   selector: 'app-profile-address',
-  imports: [CardModule, ButtonModule, SelectModule, InputTextModule, NgIcon, ReactiveFormsModule, FormsModule, CommonModule, TranslateModule, InputError],
+  imports: [
+    CardModule,
+    ButtonModule,
+    SelectModule,
+    InputTextModule,
+    NgIcon,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    TranslateModule,
+    InputError,
+  ],
   templateUrl: './profile-address.html',
   styleUrl: './profile-address.scss',
   viewProviders: [provideIcons({ featherMapPin })],
@@ -30,7 +55,7 @@ export class ProfileAddress implements OnInit {
   private fb = inject(FormBuilder);
   addressForm: FormGroup;
 
-  countries = COUNTRIES.filter((x) => x.code === 'US');
+  countries = COUNTRIES.filter(x => x.code === 'US');
   states = computed(() => {
     const country = this.currentSelectedCountry();
     if (country) {
@@ -41,7 +66,9 @@ export class ProfileAddress implements OnInit {
 
   currentSelectedCountry = signal<string | undefined>(undefined);
 
-  currentCountry = computed(() => this.countries.find((x) => x.code == this.address()?.country));
+  currentCountry = computed(() =>
+    this.countries.find(x => x.code == this.address()?.country),
+  );
 
   currentState = computed(() => {
     let address = this.address();
@@ -49,7 +76,7 @@ export class ProfileAddress implements OnInit {
       const country = address.country;
       const state = address.state;
 
-      return getStatesByCountry(country).find((x) => x.code == state);
+      return getStatesByCountry(country).find(x => x.code == state);
     }
     return null;
   });
@@ -58,12 +85,43 @@ export class ProfileAddress implements OnInit {
 
   ngOnInit(): void {
     this.addressForm = this.fb.group({
-      country: [this.address()?.country, [Validators.required, Validators.maxLength(2)]],
-      state: [this.address()?.state, [Validators.required, Validators.maxLength(2)]],
-      city: [this.address()?.city, [Validators.required, Validators.maxLength(50), AddressValidators.cityName(), AddressValidators.noWhitespaceOnly()]],
-      streetLine: [this.address()?.streetLine, [Validators.required, Validators.maxLength(256)]],
-      postalCode: [this.address()?.postalCode, [Validators.required, Validators.maxLength(10), AddressValidators.postalCode()]],
-      zipCode: [this.address()?.zipCode, [Validators.required, Validators.maxLength(10), AddressValidators.zipCode()]],
+      country: [
+        this.address()?.country,
+        [Validators.required, Validators.maxLength(2)],
+      ],
+      state: [
+        this.address()?.state,
+        [Validators.required, Validators.maxLength(2)],
+      ],
+      city: [
+        this.address()?.city,
+        [
+          Validators.required,
+          Validators.maxLength(50),
+          AddressValidators.cityName(),
+          AddressValidators.noWhitespaceOnly(),
+        ],
+      ],
+      streetLine: [
+        this.address()?.streetLine,
+        [Validators.required, Validators.maxLength(256)],
+      ],
+      postalCode: [
+        this.address()?.postalCode,
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          AddressValidators.postalCode(),
+        ],
+      ],
+      zipCode: [
+        this.address()?.zipCode,
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          AddressValidators.zipCode(),
+        ],
+      ],
     });
 
     this.currentSelectedCountry.set(this.address()?.country);
@@ -75,9 +133,11 @@ export class ProfileAddress implements OnInit {
 
   handleSubmit($event: SubmitEvent) {
     $event.preventDefault();
-    console.log('S');
     if (this.addressForm.valid) {
-      if (this.hasAddressChanged(this.addressForm.value, this.address()!)) {
+      if (
+        !this.address() ||
+        this.hasAddressChanged(this.addressForm.value, this.address()!)
+      ) {
         this.submited.emit({ address: this.addressForm.value });
       }
     }
@@ -88,6 +148,13 @@ export class ProfileAddress implements OnInit {
   }
 
   hasAddressChanged(left: Address, right: Address) {
-    return left.country != right.country || left.state != right.state || left.streetLine != right.streetLine || left.city != right.city || left.postalCode != right.postalCode || left.zipCode != right.zipCode;
+    return (
+      left.country != right.country ||
+      left.state != right.state ||
+      left.streetLine != right.streetLine ||
+      left.city != right.city ||
+      left.postalCode != right.postalCode ||
+      left.zipCode != right.zipCode
+    );
   }
 }
